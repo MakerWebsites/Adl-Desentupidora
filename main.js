b7e64c4d6c1aaef787b97d9bf513c7729150fc5a
MakerWebsites <makerwebsitess@gmail.com>
2024-09-12 11:43:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function enviarWhatsApp() {
     
 
     const mensagem = `*Nome:* ${nome}\n *Cidade:* ${cidade}\n *Bairro:* ${bairro}\n *CEP:* ${cep}\n *Rua:* ${rua}\n *Número Residência:* ${numero}\n *Serviço pedido:* ${seletorServicos}\n *Descrição do problema a ser resolvido:* ${descricaoServico}`;
-    const numeroWhatsApp = '5511968559541'; // Substitua pelo número desejado
+    const numeroWhatsApp = '+5511998729249'; // Substitua pelo número desejado
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
     window.open(url);
 }
